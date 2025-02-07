793f0d7bff47bacad33b517cee0209bbde7e5f71
Beverly <chaoscgo@hotmail.com>
2025-02-07 16:15:09 -0600

    created member table

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Member(models.Model):
+    name = models.CharField(max_length=100)
+    age = models.IntegerField()
+    gender = models.CharField(max_length=1)
+    dupr = models.IntegerField()
+    
+    def __str__(self):
+        return self.name

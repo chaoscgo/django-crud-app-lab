3fb85103015758cd1b854ba1f8686df10c9d0ace
Beverly <chaoscgo@hotmail.com>
2025-02-13 15:47:14 -0600

    added Game model

--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,13 @@
 from django.db import models
 from django.urls import reverse
 
+
+OUTCOMES = (
+    ('t', 'I won this game!'),
+    ('f', 'I lost this game.')
+)
+
+
 class Member(models.Model):
     name = models.CharField(max_length=100)
     age = models.IntegerField()
@@ -12,3 +19,16 @@ class Member(models.Model):
     
     def get_absolute_url(self):
         return reverse('member-detail', kwargs={'member_id': self.id})
+    
+class Game(models.Model):
+    date = models.DateField()
+    winner = models.BooleanField(
+        max_length = 1,
+        choices = OUTCOMES,
+        default=OUTCOMES[0][0]
+    )
+
+    member = models.ForeignKey(Member, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.get_winner_display()} on {self.date}"
